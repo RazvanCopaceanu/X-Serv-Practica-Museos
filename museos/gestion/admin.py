884a39716056647335c808a9571eb52e14664e82
RazvanCopaceanu <razvan.copaceanu19@gmail.com>
2018-06-27 17:03:18 +0200

    admin completado

--- museos/gestion/admin.py
+++ museos/gestion/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from aparcamientos.models import Aparcamiento
+from aparcamientos.models import PaginaUsuario
+from aparcamientos.models import Comentarios
+from aparcamientos.models import Elegidos
+
 
 # Register your models here.
+
+admin.site.register(Aparcamiento)
+admin.site.register(PaginaUsuario)
+admin.site.register(Comentarios)
+admin.site.register(Elegidos)
+
