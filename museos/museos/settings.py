83d6098b922343d235e8ed9b843e59223469621a
RazvanCopaceanu <razvan.copaceanu19@gmail.com>
2018-06-27 17:01:38 +0200

    settings mejorado

--- museos/museos/settings.py
+++ museos/museos/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+LOGIN_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = (
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'museos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,4 +100,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/templates/'
