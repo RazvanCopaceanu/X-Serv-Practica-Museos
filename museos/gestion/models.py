afddf889d031dc16c234a23a321865b10e6c4fc6
RazvanCopaceanu <razvan.copaceanu19@gmail.com>
2018-06-27 17:02:20 +0200

    modelo realizado

--- museos/gestion/models.py
+++ museos/gestion/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 
 class Museo(models.Model):
     nombre = models.CharField(max_length=64, default="")
