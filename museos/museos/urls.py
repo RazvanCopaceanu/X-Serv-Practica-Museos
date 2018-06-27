cddae7f1239c8909d0978546ff70bff698e1bde4
RazvanCopaceanu <razvan.copaceanu19@gmail.com>
2018-06-27 18:33:11 +0200

    annadido en urls

--- museos/museos/urls.py
+++ museos/museos/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from gestion import views
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
