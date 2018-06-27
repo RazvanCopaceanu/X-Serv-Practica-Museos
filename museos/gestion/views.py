6081507e42df448ce79590546f765ac295b4f26d
RazvanCopaceanu <razvan.copaceanu19@gmail.com>
2018-06-27 17:25:23 +0200

    views empezado

--- museos/gestion/views.py
+++ museos/gestion/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from gestion.models import Museo
+from gestion.models import PaginaUsuario
+from gestion.models import Comentarios
+from gestion.models import Elegidos
+from bs4 import BeautifulSoup
+import urllib
+from django.template.loader import get_template
+from django.template import Context, RequestContext
+from django.contrib.auth import authenticate, login, logout
+import datetime
