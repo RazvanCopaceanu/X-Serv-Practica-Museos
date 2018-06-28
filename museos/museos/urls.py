94cd75b403fc5097954938652a65d9712b39599b
RazvanCopaceanu <razvan.copaceanu19@gmail.com>
2018-06-28 08:31:13 +0200

    urls funcional

--- museos/museos/urls.py
+++ museos/museos/urls.py
@@ -21,4 +21,20 @@ from museos import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'static/terrafirma/default\.css$', views.css),
+    url(r'static/(?P<path>.*)$', serve, {'document_root':
+        '/home/razvan/SAT/X-Serv-Practica-Museos/museos/templates/'}),
+    url(r'^about$', views.about, name="Información"),
+    url(r'^$', views.main, name="Pagina principal"),
+    url(r'^xml$', views.xmlmain, name="XML de la página principal"),
+    url(r'^json$', views.json, name="JSON de la página principal"),
+    url(r'^rss$', views.rss, name="RSS de los comentarios"),
+    url(r'^museos$', views.museums, name="Página con todos los museos"),
+    url(r'^museos/(\d+)$', views.museum, name="Página de un museo"),
+    url(r'^registro$', views.registro),
+    url(r'^logout$', views.logout_view),
+    url(r'^login$', views.login_view),
+    url(r'^(.+)/xml', views.xml, name="XML de la página de usuario"),
+    url(r'^(.+)/json', views.jsonusuario, name="JSON de la página de usuario"),
+    url(r'^(.+)', views.user, name="Pagina personal del usuario"),
 ]
